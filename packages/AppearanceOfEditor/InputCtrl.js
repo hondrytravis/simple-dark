dabb82e87406bce3c9b5a3ef7832d8f5335af4a9
travis <hondrytravis.github@outlook.com>
2026-06-12 02:14:44 +0800

    feat: add borders to input fields, editor widgets, and agent chat input
    - input.border + agentsChatInput.border for input controls and Copilot agent chat
    - editorWidget.border + editor.findWidgetBorder for find/replace widget
    - widget.border for general widget border

--- packages/AppearanceOfEditor/InputCtrl.js
+++ packages/AppearanceOfEditor/InputCtrl.js
@@ -2,6 +2,8 @@ function getConfig(color) {
   return {
     // input control
     "input.background": color['ui.bg.input'],
+    "input.border": color['ui.border'],
+    "agentsChatInput.border": color['ui.border'],
   }
 }
 module.exports = getConfig
--- packages/AppearanceOfEditor/Widget.js
+++ packages/AppearanceOfEditor/Widget.js
@@ -2,6 +2,9 @@ function getConfig(color) {
   return {
     // find/replace
     "editorWidget.background": color['ui.bg.widget'],
+    "editorWidget.border": color['ui.border'],
+    "editor.findWidgetBorder": color['ui.border'],
+    "widget.border": color['ui.border'],
   }
 }
 
